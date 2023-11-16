dde70dae3a485583e56a65eb50e75ffb4810e734
François Testu <ftestu19@gmail.com>
2023-11-16 16:22:10 +0100

    commentaires en anglais

--- file.py
+++ file.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-# Charger le fichier CSV dans un DataFrame
+# Load the CSV file into a DataFrame
 df = pd.read_csv('./data/queries_train.csv')
 
-# Lire le fichier texte contenant les nouvelles queries
+# Read the text file containing the new queries
 with open('./data/queries_rewrited_train.txt', 'r') as file:
     new_queries = file.readlines()
 
-# Modifier le champ "query" dans le DataFrame avec les nouvelles queries
+# Modify the "query" field in the DataFrame with the new queries
 for i, new_query in enumerate(new_queries):
     df.at[i-1, 'query'] = new_query.strip()
 
-# Sauvegarder le DataFrame modifié dans un nouveau fichier CSV
+# Save the modified DataFrame in a new CSV file
 df.to_csv('./data/queries_rewrited_train.csv', index=False)
