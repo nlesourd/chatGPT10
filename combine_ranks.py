c213b8cd29a8cc7f9179ec24a57c15abd10cae63
François Testu <ftestu19@gmail.com>
2023-11-16 22:32:59 +0100

    combine_ranks

--- combine_ranks.py
+++ combine_ranks.py
@@ -22,6 +22,7 @@ def create_dic_qid_rank(path_file_input: str):
     return dic_qid_rank
 
 # Combine the ranks of two files with given weights for the ranks
+# We assume thats doc_ids are the same for ranking 1 and 2
 def combine_ranks(path_file_input1: str, path_file_input2: str, weight1: int, 
                   weight2: int, path_file_output: str):
     
@@ -29,11 +30,24 @@ def combine_ranks(path_file_input1: str, path_file_input2: str, weight1: int,
     dic_qid_rank1 = create_dic_qid_rank(path_file_input1)
     dic_qid_rank2 = create_dic_qid_rank(path_file_input2)
 
-    for qid in dic_qid_rank1:
-        # Retrieve all the id of documents
-        l_docs = set(list(dic_qid_rank1[qid].keys()) + list(dic_qid_rank2[qid].keys()))
-        print(len(l_docs))
+    # Put qid and docid at the top of the file
+    with open(path_file_output, 'w') as file:
+        file.write("qid,docid\n")
 
+    with open(path_file_output, 'a') as file:
+        for qid in dic_qid_rank1:
+            # Retrieve all the id of documents
+            l_docs = list(dic_qid_rank1[qid].keys())
+            # Create a list of tuples with rank and id
+            list_rank_id = []
+            for docid in l_docs:
+                new_rank = weight1 * dic_qid_rank1[qid][docid] + weight2 * dic_qid_rank2[qid][docid]
+                list_rank_id.append((new_rank, docid))
+            # Sort the list by rank
+            list_rank_id = sorted(list_rank_id, key=lambda x: x[0])
+            # Add in the doc
+            for rank, docid in list_rank_id:
+                file.write(qid + "," + docid + "\n")
 
-combine_ranks('data\kaggle_advanced_method.csv', 'data\kaggle.csv', 0.5, 
-                  0.5, 'data\combined_ranks.csv')
+combine_ranks('data\kaggle_advanced_method_v2.csv', 'data\kaggle_advanced_method.csv', 0.6, 
+                  0.4, 'data\combined_ranks.csv')
